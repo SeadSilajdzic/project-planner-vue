41de74d76b5b1ce839e7fb8c112fd77de8f002e5
SeadSilajdzic <sead.silajdzic@elevatebits.com>
2024-06-11 14:29:16 +0200

    fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
     <div class="home">
         <h1>Homepage</h1>
+        <div v-if="projects.length">
+            <div v-for="project in projects" :key="project.id">
+                {{ project.title }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'HomeView',
-    components: {}
+    name: 'Home',
+    components: {},
+    data() {
+        return {
+            projects: []
+        }
+    },
+    mounted() {
+        fetch("http://localhost:3000/projects")
+            .then(response => response.json())
+            .then(response => this.projects = response)
+            .catch(error => console.log(error));
+    }
 }
 </script>
