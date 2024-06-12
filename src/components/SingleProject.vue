81ad87274b07d4ec1bc8dec332d47e227bc883d0
SeadSilajdzic <sead.silajdzic@elevatebits.com>
2024-06-12 12:40:20 +0200

    single product complete

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -4,26 +4,43 @@ export default {
     props: ['project'],
     data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri: "http://localhost:3000/projects/" + this.project.id
         }
     },
     methods: {
         toggleDetails() {
             this.showDetails = !this.showDetails;
+        },
+
+        deleteProject() {
+            fetch(this.uri, { method: "DELETE" })
+                .then(() => this.$emit('deleteProject', this.project.id))
+                .catch(error => console.log(error));
+        },
+
+        toggleProjectComplete() {
+            fetch((this.uri), {
+                method: "PATCH",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ complete: !this.project.complete })
+            })
+                .then(() => this.$emit('toggleProjectComplete', this.project.id))
+                .catch(error => console.log(error));
         }
     }
 }
 </script>
 
 <template>
-    <div class="project">
+    <div class="project" :class="{ complete: project.complete}">
         <div class="actions">
             <h3 @click="toggleDetails">{{ project.title }}</h3>
 
             <div class="icons">
                 <span class="material-icons">edit</span>
-                <span class="material-icons">delete</span>
-                <span class="material-icons">done</span>
+                <span @click="deleteProject" class="material-icons">delete</span>
+                <span @click="toggleProjectComplete" class="material-icons" :class="{ tick: project.complete }">done</span>
             </div>
         </div>
         <div v-if="showDetails">
@@ -61,5 +78,13 @@ h3 {
 
 .material-icons:hover {
     color: #777;
+}
+
+.project.complete {
+    border-left: 4px solid #00ce89;
+}
+
+.project.complete .tick {
+    color: #00ce89;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,3 @@
-<template>
-    <div class="home">
-        <h1>Homepage</h1>
-        <div v-if="projects.length">
-            <div v-for="project in projects" :key="project.id">
-                <SingleProject :project="project"/>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script>
 import SingleProject from "@/components/SingleProject.vue";
 
@@ -25,6 +14,33 @@ export default {
             .then(response => response.json())
             .then(response => this.projects = response)
             .catch(error => console.log(error));
+    },
+    methods: {
+        handleDeleteProject(id) {
+            this.projects = this.projects.filter(project => {
+                return project.id !== id;
+            })
+        },
+
+        handleToggleProjectComplete(id) {
+            let p = this.projects.find(project => {
+                return project.id === id;
+            });
+
+            p.complete = !p.complete;
+        }
     }
 }
 </script>
+
+
+<template>
+    <div class="home">
+        <h1>Homepage</h1>
+        <div v-if="projects.length">
+            <div v-for="project in projects" :key="project.id">
+                <SingleProject :project="project" @toggleProjectComplete="handleToggleProjectComplete" @deleteProject="handleDeleteProject"/>
+            </div>
+        </div>
+    </div>
+</template>
